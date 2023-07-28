b06a735964ab61f3b2edab47363814354fbc4c58
Erikson Kaszubowski <erikson.k@ufsc.br>
2023-07-28 10:17:22 -0300

    Initial code for a simple TCP echo server

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,38 @@
 defmodule KVServer do
   @moduledoc """
-  Documentation for `KVServer`.
+  A simple server to provide KV services.
   """
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
-      iex> KVServer.hello()
-      :world
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
 
-  """
-  def hello do
-    :world
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
